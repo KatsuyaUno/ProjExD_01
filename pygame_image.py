56891cbc81fbf3e56f32f32353bc07237e6fc128
katsuya <c0a22023ef@edu.teu.ac.jp>
2023-09-26 16:00:19 +0900

    fifth commit

--- pygame_image.py
+++ pygame_image.py
@@ -13,8 +13,27 @@ def main():
     kton_img= pg.image.load("ex01/fig/3.png")
     kton_img= pg.transform.flip(kton_img, True, False) 
     kton2_img= pg.transform.rotozoom(kton_img, 10, 1.0)
+    kton3_img= pg.transform.rotozoom(kton_img, 15, 1.0)
+    kton4_img= pg.transform.rotozoom(kton_img, 20, 1.0)
+    kton5_img= pg.transform.rotozoom(kton_img, 25, 1.0)
+    kton6_img= pg.transform.rotozoom(kton_img, 30, 1.0) 
+    kton7_img= pg.transform.rotozoom(kton_img, 35, 1.0)
+    kton8_img= pg.transform.rotozoom(kton_img, 40, 1.0)
+    kton9_img= pg.transform.rotozoom(kton_img, 45, 1.0)
+    kton10_img= pg.transform.rotozoom(kton_img, 50, 1.0)
+
+
     images.append(kton_img)
-    images.append(kton2_img) 
+    images.append(kton2_img)
+    images.append(kton3_img)
+    images.append(kton4_img)
+    images.append(kton5_img)
+    images.append(kton6_img)
+    images.append(kton7_img)
+    images.append(kton8_img)
+    images.append(kton9_img)
+    images.append(kton10_img)
+
     tmr = 0
 
     x = 0
@@ -24,12 +43,14 @@ def main():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [x, 0])
-        screen.blit(images[tmr%2],[300,200])
+        screen.blit(bg_img,[1600+x,0])
+        screen.blit(images[tmr%10],[300,200])
         pg.display.update()
         tmr += 1 
         x -= 1
         if(x+1600==0):
             x = 0
+
         clock.tick(200)
 
 
