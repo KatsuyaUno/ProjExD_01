571617a83fbc06c485612256c226d14da8be4150
katsuya <c0a22023ef@edu.teu.ac.jp>
2023-09-26 16:52:49 +0900

    eigth commit

--- pygame_image.py
+++ pygame_image.py
@@ -13,25 +13,21 @@ def main():
     
     kton_img= pg.image.load("ex01/fig/3.png")
     kton_img= pg.transform.flip(kton_img, True, False) 
-    kton2_img= pg.transform.rotozoom(kton_img, 3, 1.0)
-    kton3_img= pg.transform.rotozoom(kton_img, 6, 1.0)
-    kton4_img= pg.transform.rotozoom(kton_img, 9, 1.0)
-    kton5_img= pg.transform.rotozoom(kton_img, 6, 1.0)
-    kton6_img= pg.transform.rotozoom(kton_img, 3, 1.0) 
+    kton2_img= pg.transform.rotozoom(kton_img, 10, 1.0)
+    
    
 
 
     images.append(kton_img)
     images.append(kton2_img)
-    images.append(kton3_img)
-    images.append(kton4_img)
-    images.append(kton5_img)
-    images.append(kton6_img)
+    
     
 
     tmr = 0
 
     x = 0
+    y=0
+    
 
     while True:
         for event in pg.event.get():
@@ -39,12 +35,17 @@ def main():
 
         screen.blit(bg_img, [x, 0])
         screen.blit(bg2_img,[1600+x,0])
-        screen.blit(images[tmr%6],[300,200])
+        y=tmr/50
+        int(y)
+        screen.blit(images[int(y%2)],[300,200])
+    
         pg.display.update()
         tmr += 1 
         x -= 1
         if(x+1600==0):
             x = 0
+        if(tmr>=100):
+            tmr=0
 
         clock.tick(100)
 
