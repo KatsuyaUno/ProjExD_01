20c308867b2e24f3d979c9ce5a44549409101ec3
katsuya <c0a22023ef@edu.teu.ac.jp>
2023-09-26 16:12:18 +0900

    seventh commit

--- pygame_image.py
+++ pygame_image.py
@@ -13,15 +13,12 @@ def main():
     
     kton_img= pg.image.load("ex01/fig/3.png")
     kton_img= pg.transform.flip(kton_img, True, False) 
-    kton2_img= pg.transform.rotozoom(kton_img, 10, 1.0)
-    kton3_img= pg.transform.rotozoom(kton_img, 15, 1.0)
-    kton4_img= pg.transform.rotozoom(kton_img, 20, 1.0)
-    kton5_img= pg.transform.rotozoom(kton_img, 25, 1.0)
-    kton6_img= pg.transform.rotozoom(kton_img, 30, 1.0) 
-    kton7_img= pg.transform.rotozoom(kton_img, 35, 1.0)
-    kton8_img= pg.transform.rotozoom(kton_img, 40, 1.0)
-    kton9_img= pg.transform.rotozoom(kton_img, 45, 1.0)
-    kton10_img= pg.transform.rotozoom(kton_img, 50, 1.0)
+    kton2_img= pg.transform.rotozoom(kton_img, 3, 1.0)
+    kton3_img= pg.transform.rotozoom(kton_img, 6, 1.0)
+    kton4_img= pg.transform.rotozoom(kton_img, 9, 1.0)
+    kton5_img= pg.transform.rotozoom(kton_img, 6, 1.0)
+    kton6_img= pg.transform.rotozoom(kton_img, 3, 1.0) 
+   
 
 
     images.append(kton_img)
@@ -30,10 +27,7 @@ def main():
     images.append(kton4_img)
     images.append(kton5_img)
     images.append(kton6_img)
-    images.append(kton7_img)
-    images.append(kton8_img)
-    images.append(kton9_img)
-    images.append(kton10_img)
+    
 
     tmr = 0
 
@@ -45,14 +39,14 @@ def main():
 
         screen.blit(bg_img, [x, 0])
         screen.blit(bg2_img,[1600+x,0])
-        screen.blit(images[tmr%10],[300,200])
+        screen.blit(images[tmr%6],[300,200])
         pg.display.update()
         tmr += 1 
         x -= 1
         if(x+1600==0):
             x = 0
 
-        clock.tick(200)
+        clock.tick(100)
 
 
 if __name__ == "__main__":
